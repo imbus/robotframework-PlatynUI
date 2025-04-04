c5258f12509de341edd265a14752569e28957e6e
Fabian Tsirogiannis <fabian.tsirogiannis@imbus.de>
2025-04-04 15:47:54 +0200

    feat: added x11 mouse device implementation

--- src/PlatynUI.Platform.X11/MouseDevice.cs
+++ src/PlatynUI.Platform.X11/MouseDevice.cs
@@ -9,47 +9,21 @@ using static PlatynUI.Platform.X11.Interop.XCB.XCB;
 namespace PlatynUI.Platform.X11;
 
 [Export(typeof(IMouseDevice))]
-public class MouseDevice : IMouseDevice
+[method: ImportingConstructor]
+public class MouseDevice() : IMouseDevice
 {
-    public uint Id { get; private set; }
-    public XCBConnection Connection { get; }
+    public uint Id => GetRootWindow();
+    public static XCBConnection Connection => XCBConnection.Default;
 
-    public MouseDevice(XCBConnection connection)
-    {
-        Connection = connection;
-
-        if (Connection == null)
-        {
-            throw new InvalidOperationException("Failed to initialize XCB connection.");
-        }
-
-        Id = GetRootWindow();
-    }
-
-    // In X11, there isn't a direct API to get the double-click size.
-    // Some desktop environments like GNOME store these settings in GSettings.
-    // For a generic X11 implementation, we'll return a reasonable default value.
     public Size GetDoubleClickSize()
     {
-        // Default double-click distance for X11 (varies by desktop environment)
         const int defaultDoubleClickDistance = 5; // pixels
-
-        // Try to read from the X resource database (Xrm) if available
-        // This would require interop with Xlib since XCB doesn't expose Xrm functionality
-
-        // For now, return a reasonable default value
         return new Size(defaultDoubleClickDistance, defaultDoubleClickDistance);
     }
 
     public double GetDoubleClickTime()
     {
-        // Default double-click time for X11 (varies by desktop environment)
         const double defaultDoubleClickTimeMs = 400; // milliseconds
-
-        // Try to read from the X resource database (Xrm) if available
-        // This would require interop with Xlib since XCB doesn't expose Xrm functionality
-
-        // For now, return a reasonable default value
         return defaultDoubleClickTimeMs / 1000.0; // Convert to seconds
     }
 
@@ -89,6 +63,7 @@ public class MouseDevice : IMouseDevice
             rootY: (short)y,
             deviceid: 0
         );
+        _ = xcb_flush(Connection.Connection);
     }
 
     public void Press(MouseButton button) => Press((int)button);
@@ -107,6 +82,7 @@ public class MouseDevice : IMouseDevice
             rootY: 0,
             deviceid: 0
         );
+        _ = xcb_flush(Connection.Connection);
     }
     public void Release(MouseButton button) => Release((int)button);
 
@@ -124,6 +100,7 @@ public class MouseDevice : IMouseDevice
             rootY: 0,
             deviceid: 0
         );
+        _ = xcb_flush(Connection.Connection);
     }
 
     private unsafe uint GetRootWindow()
@@ -142,10 +119,5 @@ public class MouseDevice : IMouseDevice
         return iter.data->root;
     }
 
-    ~MouseDevice()
-    {
-        // Clean up the XCB connection if necessary
-        Connection?.Dispose();
-    }
 }
 
